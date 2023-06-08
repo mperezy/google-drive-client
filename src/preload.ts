23f0bdf8c696fd75769ba3dc244e6c65fbaa25ea
Manuel Pérez <mperezy@unitru.edu.pe>
2023-06-07 21:24:16 -0500

    Implement network change events
    - When there is no Internet Network the No Internet page will be displayed
    
    - When there is Internet Network the Google Drive page will be loaded as expected

--- src/preload.ts
+++ src/preload.ts
@@ -1,2 +1,21 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
+import { ipcRenderer } from 'electron';
+import env from '../env.json';
+
+console.log('👋 This message is being logged by "preload.ts", included via webpack');
+
+const updateStatus = () => {
+  const isOnline = navigator.onLine;
+  if (env.ENV === 'development') {
+    console.log(isOnline ? 'Got internet connection. 🚀 ' : 'Not got internet connection. 💥');
+  }
+
+  ipcRenderer.send('online-status-changed', { onlineStatus: isOnline });
+};
+
+window.addEventListener('online', updateStatus);
+window.addEventListener('offline', updateStatus);
+
+updateStatus();
